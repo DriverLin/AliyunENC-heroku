d7569f06f8d5816c1f543e52502ed18d41af046e
DriverLin <lty6531600@outlook.com>
2022-10-01 20:58:54 +0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+init: 
+web: python3 main.py
---
+++ main.py
@@ -0,0 +1,33 @@
+import base64
+import hashlib
+import json
+import os
+import threading
+
+import requests
+from cryptography.fernet import Fernet
+
+
+def dec(key, data):
+    md5hash = hashlib.md5(key.encode()).hexdigest().encode('utf-8')
+    f = Fernet(base64.urlsafe_b64encode(md5hash))
+    return f.decrypt(data.encode('utf-8')).decode('utf-8')
+
+token_bin_url = "https://raw.githubusercontent.com/DriverLin/action_ruler/main/aliyunpan/auto_refresh/refresh_token.bin"
+response = requests.get(token_bin_url)
+refresh_token = dec(os.environ.get("KEY") ,response.text)
+deployAliyunWebdavCMD = "aliyundrive-webdav --refresh-token {} -p 8900 --cache-ttl 16 --debug  --upload-buffer-size 4194304".format(refresh_token)
+
+def getThread(port):
+    return [
+        threading.Thread(target=os.system, args=(deployAliyunWebdavCMD,)),
+        threading.Thread(target=os.system, args=(f"./rclone serve http aliyunenc:ADM --addr 0.0.0.0:{port} --read-only",))
+    ]
+
+if __name__ == "__main__":
+    os.system("chmod 777 ./rclone")
+    threads = getThread(os.environ.get("PORT"))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
